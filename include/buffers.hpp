842ab80c23e57aaa8817cc89ece035c9ad675335
Anna Sivula <anna.sivula@proton.me>
2025-03-08 11:34:11 +0200

    Added openGL buffer binding functions to Buffer class

--- include/buffers.hpp
+++ include/buffers.hpp
@@ -1,4 +1,6 @@
 #include <memory>
+#include <glad/glad.h> 
+#include <vector>
 
 /**
  * @class Buffers.
@@ -6,63 +8,83 @@
  *        from cpu to gpu in batches. Specific buffers inherit from this class.
  */
 class Buffers{
-    public:
-        /**
-        * @subsection Constructors
-        */
-        /**
-        * @subsection Destructors
-        */
-        /**
-        * @subsection Public Member Functions
-        */
-        
-        /**
-        * @subsection Getters
-        */
-        /**
-        * @subsection Setters
-        */
-        void generateBuffer(const unsigned int &howMany);
-    
-    protected:
-        /**
-         * @var Buffers::VBO_id
-         * @brief ID of a buffer. Default null.
-         */
-        std::unique_ptr<unsigned int> buffer_id{ nullptr };
-    
-    
-    };
-    
+public:
     /**
-     * @class VBO 
-     * @brief Vertex Buffer Object, public inheritance from Buffers.
+    * @subsection Constructors
+    */
+    /**
+    * @subsection Destructors
+    */
+    /**
+    * @subsection Public Member Functions
+    */        
+    /**
+    * @subsection Getters
+    */
+    /**
+    * @subsection Setters
+    */
+
+
+
+protected:
+    /**
+     * @subsection Protected Member Functions
      */
-    class VBO : public Buffers{
-    public:
-        /**
-        * @subsection Constructors
-        */
+    /**
+    * @name generateBuffer
+    * @brief Sets buffer_id as an unique id of a generated buffer
+    */
+    void generateBuffer();
+    /**
+     * @name bindBuffer
+     * @brief After this, any buffer calls to buffers' set by BUFFER_TYPE
+     * will reference the buffer type and it's buffer id. 
+     * @remark For example GL_ARRAY_BUFFER sets the vertex buffer, and such
+     * calls to GL_ARRAY_BUFFER will use the buffer_id of the VBO class object. 
+     */
+    void bindBuffer(const auto &BUFFER_TYPE);
+    /**
+     * @var Buffers::buffer_ids
+     * @brief A smart pointer to a vector of ID's creacted for buffers. 
+     * Default null. Creation of buffers is limited to 1.
+     */
+    std::unique_ptr<unsigned int> buffer_id{ nullptr };
+
+
+};
     
-        /**
-        * @subsection Destructors
-        */
-        /**
-        * @subsection Public Member Functions
-        */
+/**
+ * @class VBO 
+ * @brief Vertex Buffer Object, public inheritance from Buffers.
+ */
+class VBO : public Buffers{
+public:
+    /**
+    * @subsection Constructors
+    */
+    VBO(const auto vertexArray, const auto& DRAW_TYPE);
+
+    /**
+    * @subsection Destructors
+    */
+    /**
+    * @subsection Public Member Functions
+    */
+
+    /**
+    * @subsection Getters
+    */
+    /**
+    * @subsection Setters
+    */
+
+protected:
+    /**
+     * @subsection Protected Member Functions
+     */
     
-        /**
-        * @subsection Getters
-        */
-        /**
-        * @subsection Setters
-        */
-        /**
-         * @brief Initializes an object that holds space for vertices on the 
-         *        GPU's memory.
-         * @note Assigns an ID for the Vertex Buffer Object.
-         */
-        VBO();
+
+
 };
     
