be61e1c33f6237370336f86637d524e04851df42
Anna Sivula <anna.sivula@proton.me>
2025-03-08 09:58:47 +0200

    Created class buffers. Added constructor in class Buffers for a VBO, and a member to hold the ID of the created VBO.

--- triangle.hpp
+++ triangle.hpp
@@ -1,5 +1,51 @@
 #pragma once
+#include <memory>
 
 /**
 * @include Headers
 */
+
+
+/**
+ * @namespace opengl_primitives
+ * @brief Holds arrays of float values that represent primitives in 
+ * Normalized 3D Device Coordinates (values between -1.0 and 1.0)
+ */
+namespace opengl_primitives 
+{
+    float two_dimensional_triangle[] =
+    {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+
+};
+
+/**
+ * @class Buffers.
+ * @brief Datastructures in GPU's memory enabling data to be sent from cpu to 
+ *        gpu in batches.
+ * @note VBO = Vertex Buffer Object
+ */
+class Buffers
+{
+public:
+    /**
+     * @brief Initializes an object that holds space for vertices on the 
+     *        GPU's memory.
+     * @note Assigns an ID for the Vertex Buffer Object.
+     */
+    void VBO();
+
+private:
+    /**
+     * @var Buffers::VBO
+     * @brief ID of a vertex buffer. Default null.
+     * @remark It is presumed that there is only one for now but this is an 
+     *         arbitrary limit.
+     */
+    std::unique_ptr<unsigned int> VBO{ nullptr };
+
+
+};
