674d64d0945e5f82eacba9406c0567188c813323
Anna Sivula <anna.sivula@proton.me>
2025-03-08 11:56:52 +0200

    Fix buffer data upload in VBO constructor

--- src/graphics/buffers.cpp
+++ src/graphics/buffers.cpp
@@ -2,20 +2,22 @@
 
 void Buffers::generateBufferID()
 {
-    glGenBuffers(&buffer_id);
+    glGenBuffers(1, &buffer_id);
 }
 
 void VBO::bindBuffer(const GLenum GL_ARRAY_BUFFER)
 {
-    glBindBuffer(BUFFER_TYPE, &buffer_id);
+    glBindBuffer(BUFFER_TYPE, buffer_id);
 
 }
 
-VBO::VBO(const std::vector<float> &vertices, const GLenum &DRAW_TYPE=GL_STATIC_DRAW)
+
+VBO::VBO(const std::vector<float> &vertices, const GLenum &DRAW_TYPE)
 {
     generateBuffer();
     bindBuffer(GL_ARRAY_BUFFER);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexArray), vertexArray, DRAW_TYPE);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float),
+                 vertices.data(), DRAW_TYPE);
 }
 
 
