4ea21ff4a9efa1f5747a52d03bf4a408fea04a13
Anna Sivula <anna.sivula@proton.me>
2025-03-15 06:46:16 +0200

    Enhance shader.hpp with documentation and clean up comments

--- include/shaders.hpp
+++ include/shaders.hpp
@@ -1,8 +1,21 @@
+/**
+ * @file shaders.hpp
+ * @brief Header file for shader-related classes and functions.
+ */
+
 #pragma once
+
 #include <glad/glad.h>
 #include <cstddef>
 #include <stdexcept>
+#include <memory> // !!NOT USED YET!! Possibly turn char naked ptr to smart?
 
+class ShaderBase
+
+{
+public:
+
+}
 /**
  * @class VertexShader
  * @brief A class that encapsulates a simple vertex shader in OpenGL.
@@ -28,7 +41,8 @@ public:
     /**
      * @fn VertexShader::VertexShader(const char* source)
      * @brief Creates a new vertex shader object from the provided source code.
-     * @param source A constant character pointer to the GLSL source code for the vertex shader.
+     * @param source A constant character pointer to the GLSL source code for 
+     *               the vertex shader.
      */
     VertexShader(const char* source);
     /*
@@ -52,12 +66,15 @@ private:
      * the vertex shader.
      */
     const char *vertexShaderSource = "#version 330 core\n"
+    // Defining variables 
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
+    // Setting drawing position on the viewport, same as input.
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" // Output pos
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" 
     "}\0";
-    /*
+
+    /** 
      * @var VertexShader::vertexShader_
      * @brief An unsigned integer representing the compiled vertex shader.
     */
