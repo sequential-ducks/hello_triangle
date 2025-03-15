951234000cd44261ff00c07b58821327fe5e3d67
Anna Sivula <anna.sivula@proton.me>
2025-03-15 14:36:48 +0200

    Enhance documentation in buffers.hpp with detailed return descriptions and improved formatting for member functions and classes.

--- include/buffers.hpp
+++ include/buffers.hpp
@@ -1,6 +1,7 @@
 /**
  * @file buffers.hpp
  * @brief Header file for buffer processing
+ * @return void This constructor does not return a value.
  */
 #pragma once
 
@@ -13,23 +14,23 @@
  * @brief Base class for datastructures in GPU's memory enabling data to be sent 
  *        from cpu to gpu in batches. Specific buffers inherit from this class.
  */
-class Buffers{
-
+class Buffers
+{
 protected:
     /**
-     * @subsection Protected Member Functions
-     */
-    /**
-    * @name Buffers::generateBufferID
+    * @fn Buffers::generateBufferID
     * @brief Sets buffer_id as a unique OpenGL buffer ID
+    * @return void This function does not return a value.
     */
     void generateBufferID();
+
     /**
-     * @name Buffers::bindBuffer
-     * @brief After this, any buffer calls to buffers' set by BUFFER_TYPE
-     * will reference the buffer type and it's buffer id.  
+     * @var Buffers::bufferID
+     * @brief A smart pointer to IDs created for buffers. 
+     * Default null. Creation of buffers is limited to 1.
      */
-    void bindBuffer(const GLenum &BUFFER_TYPE);
+    std::unique_ptr<unsigned int> bufferID;
+
     /**
      * @var Buffers::buffer_id
      * @brief A smart pointer to IDs created for buffers. 
@@ -44,11 +45,9 @@ protected:
  * @class VBO 
  * @brief Vertex Buffer Object, public inheritance from Buffers.
  */
-class VBO : public Buffers{
+class VBO : public Buffers
+{
 public:
-    /**
-    * @subsection Constructors
-    */
     /**
      * @brief Constructs a Vertex Buffer Object (VBO) and initializes it with 
      *        vertex data.
@@ -65,26 +64,18 @@ public:
      */
     VBO(const std::vector<float>& vertices, const GLenum& DRAW_TYPE=GL_STATIC_DRAW);
 
-    /**
-    * @subsection Destructors
-    */
-    /**
-
 protected:
     /**
-     * @subsection Protected Member Functions
-     */
-    /**
-     * @name VBO::bindBuffer 
-     * @brief Sets the buffer_id binded to the vertex buffer
-     * @remark Calls to GL_ARRAY_BUFFER will use the buffer_id of the 
-     *                VBO class object.
+     * @fn void VBO::bindBuffer(const GLenum bufferType = GL_ARRAY_BUFFER);
+     * @param bufferType OpenGL buffer enum of buffer to bind.
+     * @return void This function does not return a value.
+     * @remark Calls to GL_ARRAY_BUFFER will use the buffer_id of the VBO class 
+     * object.
      */
     void bindBuffer(const GLenum bufferType = GL_ARRAY_BUFFER);
 
 private:
-
-    std::vector<float> two_dimensional_triangle_ =
+    std::vector<float> defaultTriangleVertices =
     {
         -0.5f, -0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
