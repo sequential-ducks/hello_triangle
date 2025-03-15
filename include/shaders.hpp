2735a27ecf0145c1baa11fa8d51d2e2fe5ef576b
Anna Sivula <anna.sivula@proton.me>
2025-03-15 09:52:50 +0200

    Refactor ShaderBase class to Shader class with improved documentation and structure with defined responsibilities. Added FragmentShader class. Improved VertexShader documentation and proper inheritance from Shader. Explicitly deleted copy & move assignemnts for all shaders.

--- include/shaders.hpp
+++ include/shaders.hpp
@@ -4,85 +4,129 @@
  */
 
 #pragma once
-
 #include <glad/glad.h>
 #include <cstddef>
 #include <stdexcept>
-#include <memory> // !!NOT USED YET!! Possibly turn char naked ptr to smart?
 
-class ShaderBase
+
+/**
+ * @class Shader
+ * @brief A class representing a GLSL shader.
+ * This class provides functionality to manage and compile GLSL shaders.
+ * It holds the shader source code and the compiled shader ID.
+ */
+class Shader
 {
 public:
     /**
-     * @fn ShaderBase::ShaderBase(const char* source)
-     * @brief Initializes the source member.
-     * @param source A constant character pointer to the GLSL source code for 
-     *               the shader.
+     * @fn Shader::Shader(const std::string& source)
+     * @brief Initializes source data from the provided source code.
+     * @param source The GLSL source code for the shader.
+    */
+    Shader(const char* source) : shaderSource_(source), shaderID_{0}{};
+
+    /**
+     * @fn Shader::virtual ~Shader()
+     * @brief Default virtual destructor.
      */
-    ShaderBase(const char* source);
+    virtual ~Shader() = default;
+
+    // Delete copy constructor and copy assignment operator
+    Shader(const Shader&) = delete;  
+    Shader& operator=(const Shader&) = delete;  
 
 protected:
-
-    const char *shaderSource,
-    unsigned int shaderID_;
-
-
-
-}
-/**
- * @class VertexShader
- * @brief A class that encapsulates a simple vertex shader in OpenGL.
- * 
- * This class provides a basic implementation of a vertex shader using GLSL.
- * The vertex shader simply forwards its input position to the output.
- * 
- * @remark The GLSL version used is 330 core.
- * @remark The input variable location is set to 0.
- * 
- */
-class VertexShader : protected ShaderBase
-{
-public:
     /**
-     * @fn VertexShader::VertexShader()
-     * @brief Creates a new vertex shader object and assigns its' ID to 
-     * the member vertexShader_
-     * @remark Default constructor for the VertexShader class. 
-     */
-    VertexShader();
-
-    /**
-     * @fn VertexShader::VertexShader(const char* source)
-     * @brief Creates a new vertex shader object from the provided source code.
-     * @param source A constant character pointer to the GLSL source code for 
-     *               the vertex shader.
-     */
-    VertexShader(const char* source);
-    /*
-    * @fn VertexShader::~VertexShader()
-    * @brief Default virtual destructor for the VertexShader class.
+     * @fn void Shader::checkShaderCompilation()
+     * @brief Checks if any errors happened and if a shader was compiled.
+     * @throws std::logic_error if shader compilation failed.
     */
-    virtual ~VertexShader();
-
-private:
-    /**
-     * @fn checkShaderCompilation
-     * @brief Checks if any errors happened and if a shader
-     * was compiled. 
-     * @throws Logic error if shader compilation failed
-     */
     void checkShaderCompilation();
 
     /**
-     * @var VertexShader::vertexShaderSource
-     * @brief A constant character pointer containing the GLSL source code for 
-     * the vertex shader.
+     * @fn void Shader::compileShader();
+     * @brief Compiles the shader from the provided source code that also 
+     * determines the type of the shader.
+     * @note Requires that shaderID_ is set properly to a nonzero value.
+    */
+    void compileShader();
+
+    /**
+     * @var shaderSource_
+     * @brief Contains the GLSL source code for the shader.
      */
-    const char *vertexShaderSource;
+    const char* shaderSource_;
 
     /** 
-     * @var VertexShader::vertexShader_
-     * @brief An unsigned integer representing the compiled vertex shader.
+     * @var shaderID_
+     * @brief An unsigned integer representing the compiled shader.
     */
-    unsigned int vertexShaderID_;
+    unsigned int shaderID_;
+};
+
+
+/**
+ * @class VertexShader
+ * @brief A class representing a vertex shader.
+ * 
+ * This class is derived from the Shader class and is used to handle vertex shaders.
+ * It provides functionality to construct a vertex shader with given source code
+ * and to generate an ID for the shader.
+**/
+class VertexShader : protected Shader
+{
+public:
+    /**
+     * @fn VertexShader::VertexShader(const char* source)
+     * @brief Constructs a VertexShader object with the given source code.
+     * @param source The source code for the vertex shader.
+     */
+    VertexShader(const char* source);
+
+    /**
+     * @fn VertexShader::~VertexShader()
+     * @brief Default destructor for the VertexShader class.
+    */
+    ~VertexShader() = default;
+
+    // Delete copy constructor and copy assignment operator
+    VertexShader(const VertexShader&) = delete;  
+    VertexShader& operator=(const VertexShader&) = delete;  
+
+protected:
+    /**
+     * @fn void VertexShader::generateID()
+     * @brief Generates an ID for the vertex shader.
+     */
+    void generateID();
+
+};
+
+class FragmentShader : protected Shader
+{
+public:
+    /**
+     * @fn FragmentShader::FragmentShader(const char* source);
+     * @brief Constructs a FragmentShader object with the given source code.
+     * @param source The source code for the vertex shader.
+     */
+    FragmentShader(const char* source);
+
+    /**
+     * @fn FragmentShader::~FragmentShader()
+     * @brief Default destructor for the FragmentShader class.
+    */
+    ~FragmentShader() = default;
+
+    // Delete copy constructor and copy assignment operator
+    FragmentShader(const FragmentShader&) = delete;  
+    FragmentShader& operator=(const FragmentShader&) = delete;  
+
+protected:
+    /**
+     * @fn void FragmentShader::generateID()
+     * @brief Generates an ID for the frag shader.
+     */
+    void generateID();
+
 };
