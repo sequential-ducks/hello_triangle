08819c1eabc179b1ce7e205737bd34daa88136b4
Anna Sivula <anna.sivula@proton.me>
2025-03-15 14:37:40 +0200

    Refactor Shader class to improve structure and functionality. Added linkProgram method, enhanced ID generation, and updated shader compilation checks. Improved documentation for clarity.

--- include/shaders.hpp
+++ include/shaders.hpp
@@ -7,7 +7,8 @@
 #include <glad/glad.h>
 #include <cstddef>
 #include <stdexcept>
-
+#include <vector>
+#include <iostream>
 
 /**
  * @class Shader
@@ -22,34 +23,70 @@ public:
      * @fn Shader::Shader(const std::string& source)
      * @brief Initializes source data from the provided source code.
      * @param source The GLSL source code for the shader.
+     * @return void This function does not return a value.
     */
-    Shader(const char* source) : shaderSource_(source), shaderID_{0}{};
+    Shader(const char* source) : shaderSource_(source), shaderID_{0}, 
+                                 programID_{0}{};
 
     /**
      * @fn Shader::virtual ~Shader()
      * @brief Default virtual destructor.
+     * @return void This function does not return a value.
      */
     virtual ~Shader() = default;
 
-    // Delete copy constructor and copy assignment operator
+    // Delete copy constructor and copy assignment operator.
     Shader(const Shader&) = delete;  
-    Shader& operator=(const Shader&) = delete;  
+    Shader& operator=(const Shader&) = delete;
+
+    /**
+     * @fn unsigned int Shader::linkProgram
+     * @brief Attaches shaders to form a linked shader program.
+     * @return unsigned int The ID of a linked program.
+     * @throws 
+     */
+    unsigned int linkProgram();
 
 protected:
+
     /**
-     * @fn void Shader::checkShaderCompilation()
+     * @fn void Shader::generateID(GLenum shaderType);
+     * @brief Generates a shader ID for the specified shader type.
+     * This function creates a new shader object and assigns it an ID based on 
+     * the provided shader type. The shader type must be one of the valid 
+     * OpenGL shader types, such as GL_VERTEX_SHADER or GL_FRAGMENT_SHADER.
+     * @param shaderType The type of shader to generate. 
+     * Must be a valid GLenum value representing a shader type.
+     * @return void This function does not return a value.
+     */
+    void generateID(GLenum shaderType);
+
+    /**
+     * @fn void Shader::checkShaderCompilation(const std::string &shaderType);
      * @brief Checks if any errors happened and if a shader was compiled.
      * @throws std::logic_error if shader compilation failed.
+     * @return void This function does not return a value.
     */
-    void checkShaderCompilation();
+    void checkShaderCompilation(const std::string &shaderType);
 
     /**
      * @fn void Shader::compileShader();
-     * @brief Compiles the shader from the provided source code that also 
+     * @brief Compiles the shader from the provided source code that also
      * determines the type of the shader.
      * @note Requires that shaderID_ is set properly to a nonzero value.
-    */
+     * @return void This function does not return a value.
+     */
     void compileShader();
+
+    void generateProgramID();
+    /**
+     * @fn void Shader::checkProgramCompilation();
+     * @brief Checks if any errors happened and if a program was compiled.
+     * @throws std::logic_error if program compilation failed.
+     * @return void This function does not return a value.
+    */
+    void checkProgramCompilation();
+
 
     /**
      * @var shaderSource_
@@ -59,9 +96,22 @@ protected:
 
     /** 
      * @var shaderID_
-     * @brief An unsigned integer representing the compiled shader.
+     * @brief An unsigned integer representing a compiled shader from a 
+     * derived class object.
     */
     unsigned int shaderID_;
+
+    /** 
+     * @var shaders_
+     * @brief Container for all compiled shaders from deriving classes.
+    */
+    std::vector<unsigned int> shaders_;
+
+    /**
+     * @var programID_
+     * @brief Holds the ID of the linked program.
+     */
+    unsigned int programID_;
 };
 
 
@@ -73,19 +123,21 @@ protected:
  * It provides functionality to construct a vertex shader with given source code
  * and to generate an ID for the shader.
 **/
-class VertexShader : protected Shader
+class VertexShader : public Shader
 {
 public:
     /**
      * @fn VertexShader::VertexShader(const char* source)
      * @brief Constructs a VertexShader object with the given source code.
      * @param source The source code for the vertex shader.
+     * @return void This function does not return a value.
      */
     VertexShader(const char* source);
 
     /**
      * @fn VertexShader::~VertexShader()
      * @brief Default destructor for the VertexShader class.
+     * @return void This function does not return a value.
     */
     ~VertexShader() = default;
 
@@ -93,40 +145,28 @@ public:
     VertexShader(const VertexShader&) = delete;  
     VertexShader& operator=(const VertexShader&) = delete;  
 
-protected:
-    /**
-     * @fn void VertexShader::generateID()
-     * @brief Generates an ID for the vertex shader.
-     */
-    void generateID();
-
 };
 
-class FragmentShader : protected Shader
+class FragmentShader : public Shader
 {
 public:
     /**
      * @fn FragmentShader::FragmentShader(const char* source);
      * @brief Constructs a FragmentShader object with the given source code.
      * @param source The source code for the vertex shader.
+     * @return void This function does not return a value.
      */
     FragmentShader(const char* source);
 
     /**
      * @fn FragmentShader::~FragmentShader()
      * @brief Default destructor for the FragmentShader class.
+     * @return void This function does not return a value.
     */
     ~FragmentShader() = default;
 
     // Delete copy constructor and copy assignment operator
     FragmentShader(const FragmentShader&) = delete;  
     FragmentShader& operator=(const FragmentShader&) = delete;  
-
-protected:
-    /**
-     * @fn void FragmentShader::generateID()
-     * @brief Generates an ID for the frag shader.
-     */
-    void generateID();
 
 };
--- src/graphics/shaders.cpp
+++ src/graphics/shaders.cpp
@@ -1,7 +1,22 @@
 #include "shaders.hpp"
 
+void Shader::generateID(GLenum shaderType)
+{
+    shaderID_ = glCreateShader(shaderType);
+    shaders_.push_back(shaderID_);
+}
 
-void Shader::checkShaderCompilation()
+void Shader::compileShader()
+{
+    if (shaderID_ != 0)
+    {
+        glShaderSource(shaderID_, 1, &shaderSource_, NULL);
+        glCompileShader(shaderID_);
+    }
+}
+
+
+void Shader::checkShaderCompilation(const std::string& shaderType)
 {
     int success;
     char infoLog[512];
@@ -9,41 +24,62 @@ void Shader::checkShaderCompilation()
     if (!success)
     {
         glGetShaderInfoLog(shaderID_, 512, NULL, infoLog);
-        throw std::logic_error(std::string("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n ") 
+        throw std::logic_error(std::string("ERROR::SHADER::") + shaderType + 
+                                std::string("::COMPILATION_FAILED\n ") 
                                 + infoLog);
     }
 }
 
-void Shader::compileShader()
+void Shader::generateProgramID()
 {
-    if (shaderID_)
+    programID_ = glCreateProgram();
+}
+
+void Shader::checkProgramCompilation()
+{
+    int success;
+    char infoLog[512];
+    glGetProgramiv(programID_, GL_LINK_STATUS, &success);
+    if (!success)
     {
-        glShaderSource(shaderID_, 1, &shaderSource_, NULL);
-        glCompileShader(shaderID_);
+        glGetProgramInfoLog(programID_, 512, NULL, infoLog);
+        throw std::logic_error(std::string("ERROR::PROGRAM::LINKING_FAILED\n") 
+                                + infoLog);
     }
 }
 
-void VertexShader::generateID()
+unsigned int Shader::linkProgram()
 {
-    shaderID_ = glCreateShader(GL_VERTEX_SHADER);
+    generateProgramID();
+    if (programID_ != 0 and !shaders_.empty())
+    {
+        for (const auto &s : shaders_)
+        {
+            glAttachShader(programID_, s);
+            glDeleteShader(s);
+        }
+        glLinkProgram(programID_);
+    }
+    checkProgramCompilation();
+    glUseProgram(programID_);
+    return programID_;
 }
 
-VertexShader::VertexShader(const char *source) : Shader(source)
+
+VertexShader::VertexShader(const char *source) 
+    : Shader(source)
 {
-    generateID();
+    generateID(GL_VERTEX_SHADER);
     compileShader();
-    checkShaderCompilation();
+    checkShaderCompilation("VERTEX");
 }
 
 
-FragmentShader::FragmentShader(const char *source) : Shader(source)
+FragmentShader::FragmentShader(const char *source) 
+    : Shader(source)
 {
-    generateID();
+    generateID(GL_FRAGMENT_SHADER);
     compileShader();
-    checkShaderCompilation();
+    checkShaderCompilation("FRAGMENT");
 }
 
-void FragmentShader::generateID()
-{
-    shaderID_ = glCreateShader(GL_FRAGMENT_SHADER);
-}
